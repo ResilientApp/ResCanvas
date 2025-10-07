28e941809e57e059e9b1915055deaceec33cf79f
bchou9 <bchou9@gmail.com>
2025-10-07 02:02:56 +0000

    reorg: add legacy wrappers and toolbar shim; move UI components into components/

---
+++ frontend/src/_legacy/drawModeMenu.js
@@ -0,0 +1,2 @@
+// Legacy copy of drawModeMenu kept for reference during reorg.
+export { default } from '../drawModeMenu';
---
+++ frontend/src/_legacy/shapeMenu.js
@@ -0,0 +1,2 @@
+// Legacy copy of shapeMenu kept for reference during reorg.
+export { default } from '../shapeMenu';
