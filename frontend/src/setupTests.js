10fb6f1c11610dfe4a682c6ffc79c27c537dce4d
bchou9 <bchou9@gmail.com>
2025-10-07 02:17:41 +0000

    reorg(test): add canvas getContext polyfill in setupTests to stabilize Jest

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,31 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Test environment canvas polyfill: some components call canvas.getContext('2d')
+// during mount; in Jest's jsdom this can be null. Provide a minimal mock
+// to prevent TypeError during tests. This is a safe, test-only shim and
+// does not affect production code.
+if (typeof HTMLCanvasElement !== 'undefined' && !HTMLCanvasElement.prototype.getContext) {
+	HTMLCanvasElement.prototype.getContext = function () {
+		return {
+			clearRect: () => {},
+			getImageData: (x, y, w, h) => ({ data: new Uint8ClampedArray(w * h * 4) }),
+			putImageData: () => {},
+			toDataURL: () => '',
+			drawImage: () => {},
+			fillRect: () => {},
+			stroke: () => {},
+			beginPath: () => {},
+			moveTo: () => {},
+			lineTo: () => {},
+			arc: () => {},
+			fill: () => {},
+			strokeStyle: '',
+			fillStyle: '',
+			lineWidth: 1,
+			translate: () => {},
+			scale: () => {},
+		};
+	};
+}
