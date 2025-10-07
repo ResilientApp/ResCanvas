83780ee263e68f9e8b01ca5288d18a453e946cae
bchou9 <bchou9@gmail.com>
2025-10-07 02:18:12 +0000

    reorg(test): strengthen canvas getContext shim to use existing ctx if available

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -5,11 +5,20 @@
 import '@testing-library/jest-dom';
 
 // Test environment canvas polyfill: some components call canvas.getContext('2d')
-// during mount; in Jest's jsdom this can be null. Provide a minimal mock
+// during mount; in Jest's jsdom this can return null. Provide a minimal mock
 // to prevent TypeError during tests. This is a safe, test-only shim and
 // does not affect production code.
-if (typeof HTMLCanvasElement !== 'undefined' && !HTMLCanvasElement.prototype.getContext) {
-	HTMLCanvasElement.prototype.getContext = function () {
+if (typeof HTMLCanvasElement !== 'undefined') {
+	const origGetContext = HTMLCanvasElement.prototype.getContext;
+	HTMLCanvasElement.prototype.getContext = function (type) {
+		try {
+			const ctx = origGetContext ? origGetContext.call(this, type) : null;
+			if (ctx) return ctx;
+		} catch (e) {
+			// fallthrough to provide mock
+		}
+
+		// Minimal 2D context mock used only by tests
 		return {
 			clearRect: () => {},
 			getImageData: (x, y, w, h) => ({ data: new Uint8ClampedArray(w * h * 4) }),
@@ -28,6 +37,7 @@ if (typeof HTMLCanvasElement !== 'undefined' && !HTMLCanvasElement.prototype.get
 			lineWidth: 1,
 			translate: () => {},
 			scale: () => {},
+			measureText: () => ({ width: 0 }),
 		};
 	};
 }
